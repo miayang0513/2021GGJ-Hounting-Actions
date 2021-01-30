17d96e18e5a40c095d584c3a8e9bee756c074837
miayang0513 <a16168972@gmail.com>
2021-01-31 01:16:59 +0800

    feat: detect available

--- src/store/index.js
+++ src/store/index.js
@@ -12,16 +12,44 @@ const actions = {
       notificationElement.remove()
     })
     document.querySelector('body')?.prepend(notificationElement)
+  },
+  makeItemJitter (context, availableItems) {
+    const statusList = context.state.items.map(itemId => {
+      if (itemId === null) {
+        return 'empty'
+      }
+      return availableItems.map(item => item.in).includes(itemId) ? 'available' : 'exist'
+    })
+    context.dispatch('setBaggageStatus', {
+      statusList,
+      clickEvent: (index) => {
+        context.dispatch('useItem', context.state.items[index])
+      }
+    })
+  },
+  cancelItemJitter (context) {
+    const statusList = context.state.items.map(itemId => itemId === null ? 'empty' : 'exist')
+    context.dispatch('setBaggageStatus', { statusList, clickEvent: null })
+  },
+  setBaggageStatus (context, { statusList, clickEvent }) {
+    const baggageElement = document.querySelector('.baggage')
+    for (let i = 0; i < baggageElement.children.length; i++) {
+      const element = baggageElement.children[i]
+      element.onclick = clickEvent?.bind(this, [i])
+      element.dataset.status = statusList[i]
+    }
+  },
+  useItem (context, id) {
+    console.log('use item', id)
   }
 }
 
-const mutations = {
-}
+const mutations = {}
 
 const initialState = {
   name: 'SNOW',
   stamina: 30,
-  items: [],
+  items: ['rope', 'necklace', 'umbrella', 'wine-bottle'],
 }
 
 export default new Store({
